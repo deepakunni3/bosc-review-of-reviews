f501ca8770453aab92b934511afd1f69a974db24
Deepak <deepakunni3@users.noreply.github.com>
2023-05-17 16:35:55 +0200

    Tidy up and fix formatting

--- collate_reviews.py
+++ collate_reviews.py
@@ -7,13 +7,25 @@ from pathlib import Path
 
 
 KNOWN_PROPERTIES = {
-    "PC member", "Time", "Overall evaluation", 
-    "Reviewer's confidence", "Confidential remarks for the program committee", "Relevant",
-    "Available", "Open", "Updated", "Formatted", "Community", "Novelty", "Example", 
-    "Runnable", "Quality score", "Suitability for a long talk", "Confidential remarks for the chair (optional)"
+    "PC member",
+    "Time",
+    "Overall evaluation",
+    "Reviewer's confidence",
+    "Confidential remarks for the program committee",
+    "Relevant",
+    "Available",
+    "Open",
+    "Updated",
+    "Formatted",
+    "Community",
+    "Novelty",
+    "Example",
+    "Runnable",
+    "Quality score",
+    "Suitability for a long talk",
+    "Confidential remarks for the chair (optional)",
 }
-SKIPPED_PROPERTIES = {'Acceptance criteria', 'Additional desiderata', 'Overall rating and comments'}
-
+SKIPPED_PROPERTIES = {"Acceptance criteria", "Additional desiderata", "Overall rating and comments"}
 
 
 def process_submission(submission_block: List) -> Dict:
@@ -32,7 +44,7 @@ def process_submission(submission_block: List) -> Dict:
     current_property = None
     submission_id = None
     for element in submission_block:
-        if element.startswith("[") and ' et al' not in element:
+        if element.startswith("[") and " et al" not in element:
             # parse title
             m = re.search("\[\d{1,9}\]", element)
             submission_id = m.group()[1:-1]
@@ -138,7 +150,10 @@ def process_properties(submission_id, review, property_block) -> Dict:
                 continue
             if not property_name:
                 print(f"element has no accompanying property_name: '{element}'")
-            if property_name not in {"Confidential remarks for the chair (optional)", "Confidential remarks for the program committee"}:
+            if property_name not in {
+                "Confidential remarks for the chair (optional)",
+                "Confidential remarks for the program committee",
+            }:
                 field = f"{property_name} remarks"
             else:
                 field = property_name
@@ -158,19 +173,30 @@ def write_tsv(data: List, filename: str) -> None:
         filename: The output filename
 
     """
-    ordered_fields = ('Time', 'submission_id', 'submission_authors', 'submission_title', 'review_id', 'PC member', 'score_summary', 'remark_summary')
+    ordered_fields = (
+        "Time",
+        "submission_id",
+        "submission_authors",
+        "submission_title",
+        "review_id",
+        "PC member",
+        "score_summary",
+        "remark_summary",
+    )
     ordered_formatted_fields = {
-        'Time': 'Time',
-        'submission_id': 'Submission ID',
-        'submission_authors': 'Submission Authors',
-        'submission_title': 'Submission Title',
-        'review_id': 'Review ID',
-        'PC member': 'PC Member',
-        'score_summary': 'Scores',
-        'remark_summary': 'Remarks'
+        "Time": "Time",
+        "submission_id": "Submission ID",
+        "submission_authors": "Submission Authors",
+        "submission_title": "Submission Title",
+        "review_id": "Review ID",
+        "PC member": "PC Member",
+        "score_summary": "Scores",
+        "remark_summary": "Remarks",
     }
-    with open(filename, 'w') as FH:
-        writer = csv.DictWriter(FH, fieldnames=list(ordered_formatted_fields.values()), delimiter='\t')
+    with open(filename, "w") as FH:
+        writer = csv.DictWriter(
+            FH, fieldnames=list(ordered_formatted_fields.values()), delimiter="\t"
+        )
         writer.writeheader()
         for obj in data:
             record = {ordered_formatted_fields[key]: obj[key] for key in ordered_fields}
@@ -182,7 +208,7 @@ def main(input_filename: Path, output_filename: Path):
     submission_block = []
     with open(input_filename, encoding="utf-8") as FH:
         for line in FH:
-            if line.startswith("[") and ' et al' not in line:
+            if line.startswith("[") and " et al" not in line:
                 submission = process_submission(submission_block)
                 submissions.append(submission)
                 submission_block = [line.rstrip()]
@@ -199,29 +225,45 @@ def main(input_filename: Path, output_filename: Path):
             for review in submission["reviews"]:
                 # Available	Available remarks	Community	Community remarks	Confidential remarks for the chair (optional)	Confidential remarks for the program committee	Example	Example remarks	Formatted	Formatted remarks	Novelty	Novelty remarks	Open	Open remarks	Overall evaluation	Overall evaluation remarks	PC member	Quality score	Quality score remarks	Relevant	Relevant remarks	Reviewer's confidence	Runnable	Runnable remarks	Suitability for a long talk	Suitability for a long talk remarks	Time	Updated	Updated remarks	review_id	submission_authors	submission_id	submission_title
                 score_summary = [
-                    f'Overall evaluation: {review["Overall evaluation"]}', f"Reviewer's confidence: " + review["Reviewer's confidence"], f'Relevant: {review["Relevant"]}',
-                    f'Available: {review["Available"]}', f'Open: {review["Open"]}', f'Updated: {review["Updated"]}',
-                    f'Formatted: {review["Formatted"]}', f'Community: {review["Community"]}', f'Novelty: {review["Novelty"]}',
-                    f'Example: {review["Example"]}', f'Runnable: {review["Runnable"]}', f'Quality score: {review["Quality score"]}', f'Suitability for a long talk: {review["Suitability for a long talk"]}'
+                    f'Overall evaluation: {review["Overall evaluation"]}',
+                    f"Reviewer's confidence: " + review["Reviewer's confidence"],
+                    f'Relevant: {review["Relevant"]}',
+                    f'Available: {review["Available"]}',
+                    f'Open: {review["Open"]}',
+                    f'Updated: {review["Updated"]}',
+                    f'Formatted: {review["Formatted"]}',
+                    f'Community: {review["Community"]}',
+                    f'Novelty: {review["Novelty"]}',
+                    f'Example: {review["Example"]}',
+                    f'Runnable: {review["Runnable"]}',
+                    f'Quality score: {review["Quality score"]}',
+                    f'Suitability for a long talk: {review["Suitability for a long talk"]}',
                 ]
                 score_summary_str = "\n\n".join(score_summary)
 
                 remark_summary = [
-                    f'Overall evaluation: {review["Overall evaluation remarks"]}', f"Confidential remarks for the program committee " + review["Confidential remarks for the program committee"], 
+                    f'Overall evaluation: {review["Overall evaluation remarks"]}',
+                    f"Confidential remarks for the program committee "
+                    + review["Confidential remarks for the program committee"],
                     f'Relevant: {review["Relevant remarks"]}',
-                    f'Available: {review["Available remarks"]}', f'Open: {review["Open remarks"]}', f'Updated: {review["Updated remarks"]}',
-                    f'Formatted: {review["Formatted remarks"]}', f'Community: {review["Community remarks"]}', f'Novelty: {review["Novelty remarks"]}',
-                    f'Example: {review["Example remarks"]}', f'Runnable: {review["Runnable remarks"]}', f'Quality score: {review["Quality score remarks"]}',
-                    f'Suitability for a long talk: {review["Suitability for a long talk remarks"]}'
+                    f'Available: {review["Available remarks"]}',
+                    f'Open: {review["Open remarks"]}',
+                    f'Updated: {review["Updated remarks"]}',
+                    f'Formatted: {review["Formatted remarks"]}',
+                    f'Community: {review["Community remarks"]}',
+                    f'Novelty: {review["Novelty remarks"]}',
+                    f'Example: {review["Example remarks"]}',
+                    f'Runnable: {review["Runnable remarks"]}',
+                    f'Quality score: {review["Quality score remarks"]}',
+                    f'Suitability for a long talk: {review["Suitability for a long talk remarks"]}',
                 ]
                 remark_summary_str = "\n\n".join(remark_summary)
                 review["score_summary"] = score_summary_str
                 review["remark_summary"] = remark_summary_str
                 all_reviews.append(review)
 
-    #print(json.dumps(all_reviews, indent=2))
+    # print(json.dumps(all_reviews, indent=2))
     write_tsv(data=all_reviews, filename=output_filename)
-
 
 
 if __name__ == "__main__":
